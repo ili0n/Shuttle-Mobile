f835ed09db01a99d40dd67a78b71eeafe15da9f0
magley <60552482+magley@users.noreply.github.com>
2022-11-11 12:03:59 +0100

    feat: Add toolbar menu items

--- shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/DriverMainActivity.java
+++ shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/DriverMainActivity.java
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.shuttlemobile.R;
 
@@ -20,6 +23,8 @@ public class DriverMainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        toolbar.setOnMenuItemClickListener(item -> toolbarOnItemClick(item));
+
         setVisibleFragment(DriverMainFragment.newInstance());
     }
 
@@ -27,5 +32,33 @@ public class DriverMainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).replace(R.id.driver_fragment_frame, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private boolean toolbarOnItemClick(MenuItem item) {
+        final int itemId = item.getItemId();
+
+        switch (itemId) {
+            case R.id.driver_main_menu_home:
+                setVisibleFragment(DriverMainFragment.newInstance());
+                break;
+            case R.id.driver_main_menu_history:
+                setVisibleFragment(DriverRideHistoryFragment.newInstance());
+                break;
+            case R.id.driver_main_menu_inbox:
+                Toast.makeText(this, "TODO: Inbox Fragment", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.driver_main_menu_account:
+                Toast.makeText(this, "TODO: Account Fragment", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 }
