cfc87052343c65070593f44978af5d44a8b5cde6
unknown <nemanja.majstorovic3214@gmail.com>
2022-11-12 19:20:30 +0100

    Added passenger account

---
+++ shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/Passenger.java
@@ -0,0 +1,76 @@
+package com.example.shuttlemobile.passenger;
+
+public class Passenger {
+    private int id;
+    private String name;
+    private String surname;
+    private String phone;
+    private String email;
+    private String address;
+    private String password;
+    private boolean blocked;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+}
