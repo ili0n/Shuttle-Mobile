6c9e3044143f14538b52e1ef146dcc80f2264dc1
unknown <nemanja.majstorovic3214@gmail.com>
2022-11-13 15:51:44 +0100

    Added reserving routes

--- shuttle-mobile/app/src/main/java/com/example/shuttlemobile/route/FavouriteRoutesActivity.java
+++ shuttle-mobile/app/src/main/java/com/example/shuttlemobile/route/FavouriteRoutesActivity.java
@@ -2,7 +2,12 @@ package com.example.shuttlemobile.route;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.shuttlemobile.R;
@@ -24,13 +29,31 @@ public class FavouriteRoutesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_favourite_routes);
 
         favouriteRoutes = new ArrayList<Route>();
-        favouriteRoutes.add(
-                new Route(123, LocalDateTime.now(), LocalDateTime.now(),
-                        LocalTime.now(ZoneId.systemDefault()),123, 2,
-                        new Location(123, 123), new Location(500, 500)));
+        addMockData();
 
         lvFavouriteRoutes = findViewById(R.id.lvFavouriteRoutes);
-        lvFavouriteRoutes.setAdapter(new FavouriteRouteAdapter(favouriteRoutes,FavouriteRoutesActivity.this));
+        lvFavouriteRoutes.setAdapter(new FavouriteRouteAdapter(favouriteRoutes, FavouriteRoutesActivity.this));
+
+        lvFavouriteRoutes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Route route = (Route) adapterView.getItemAtPosition(position);
+                new AlertDialog.Builder(FavouriteRoutesActivity.this).setTitle("Do you want to reserve this route now?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Reserving logic
+                            }
+                        })
+                        .setNegativeButton("No", null).show();
+            }
+        });
+    }
+
+    private void addMockData() {
+        favouriteRoutes.add(
+                new Route(123, LocalDateTime.now(), LocalDateTime.now(),
+                        LocalTime.now(ZoneId.systemDefault()), 123, 2,
+                        new Location(123, 123), new Location(500, 500)));
     }
 
     @Override
