1e680a0a7dc04aa15ac45fac7e0cf6b0767be58c
unknown <nemanja.majstorovic3214@gmail.com>
2022-11-13 14:07:42 +0100

    Added save button

--- shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/Driver.java
+++ shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/Driver.java
@@ -3,4 +3,10 @@ package com.example.shuttlemobile.driver;
 import com.example.shuttlemobile.user.User;
 
 public class Driver extends User {
+    public Driver(int id, String name, String surname, String phone, String email, String password, boolean blocked, String address) {
+        super(id, name, surname, phone, email, password, blocked, address);
+    }
+    public Driver(){
+        super();
+    }
 }
--- shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/PassengerAccountFragment.java
+++ shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/PassengerAccountFragment.java
@@ -17,12 +17,22 @@ import android.widget.ImageView;
 import com.example.shuttlemobile.R;
 
 public class PassengerAccountFragment extends Fragment {
-    private ImageView addressEdit;
-    private ImageView nameEdit;
-    private ImageView surnameEdit;
-    private ImageView phoneEdit;
-    private ImageView emailEdit;
+    private ImageView addressEditImage;
+    private ImageView nameEditImage;
+    private ImageView surnameEditImage;
+    private ImageView phoneEditImage;
+    private ImageView emailEditImage;
     private Button btnSavePassenger;
+    
+    private Passenger passenger = new Passenger(123, "Pera", "Peric",
+            "0642314", "pera@gmail.com", "perica123", false,
+            "Sime simica 10");
+    private EditText etAddress;
+    private EditText etName;
+    private EditText etSurname;
+    private EditText etPhone;
+    private EditText etEmail;
+
     public static PassengerAccountFragment newInstance() {
         return new PassengerAccountFragment();
     }
@@ -41,37 +51,75 @@ public class PassengerAccountFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        addressEdit = view.findViewById(R.id.addressEdit);
-        nameEdit = view.findViewById(R.id.nameEdit);
-        surnameEdit = view.findViewById(R.id.surnameEdit);
-        phoneEdit = view.findViewById(R.id.phoneEdit);
-        emailEdit = view.findViewById(R.id.emailEdit);
-        btnSavePassenger = view.findViewById(R.id.btnSavePassenger);
+        init(view);
+    }
+
+    private void init(View view) {
+        initImageViews(view);
+        initEditTexts(view);
+        setPassenger(view);
+        setListeners(view);
+    }
 
 
-        addressEdit.setOnClickListener(new EditListener(view, R.id.addressValue));
-        nameEdit.setOnClickListener(new EditListener(view, R.id.nameValue));
-        surnameEdit.setOnClickListener(new EditListener(view, R.id.surnameValue));
-        phoneEdit.setOnClickListener(new EditListener(view, R.id.phoneValue));
-        emailEdit.setOnClickListener(new EditListener(view, R.id.emailValue));
+    private void setListeners(View view) {
+        addressEditImage.setOnClickListener(new EditImageListener(view, R.id.addressValue));
+        nameEditImage.setOnClickListener(new EditImageListener(view, R.id.nameValue));
+        surnameEditImage.setOnClickListener(new EditImageListener(view, R.id.surnameValue));
+        phoneEditImage.setOnClickListener(new EditImageListener(view, R.id.phoneValue));
+        emailEditImage.setOnClickListener(new EditImageListener(view, R.id.emailValue));
         btnSavePassenger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View btn) {
-                addressEdit.setEnabled(false);
-                nameEdit.setEnabled(false);
-                surnameEdit.setEnabled(false);
-                emailEdit.setEnabled(false);
-                phoneEdit.setEnabled(false);
+                passenger.setAddress(etAddress.getText().toString());
+                passenger.setEmail(etEmail.getText().toString());
+                passenger.setName(etName.getText().toString());
+                passenger.setPhone(etPhone.getText().toString());
+                passenger.setSurname(etSurname.getText().toString());
+
+                etAddress.setEnabled(false);
+                etName.setEnabled(false);
+                etSurname.setEnabled(false);
+                etEmail.setEnabled(false);
+                etPhone.setEnabled(false);
+
+                btnSavePassenger.setVisibility(View.INVISIBLE);
             }
         });
     }
 
-    class EditListener implements View.OnClickListener {
+    private void setPassenger(View view) {
+        etAddress.setText(passenger.getAddress());
+        etName.setText(passenger.getName());
+        etSurname.setText(passenger.getSurname());
+        etPhone.setText(passenger.getPhone());
+        etEmail.setText(passenger.getEmail());
+    }
+
+    private void initEditTexts(View view) {
+        etAddress = view.findViewById(R.id.addressValue);
+        etName = view.findViewById(R.id.nameValue);
+        etSurname = view.findViewById(R.id.surnameValue);
+        etPhone = view.findViewById(R.id.phoneValue);
+        etEmail = view.findViewById(R.id.emailValue);
+        btnSavePassenger = view.findViewById(R.id.btnSavePassenger);
+    }
+
+    private void initImageViews(View view) {
+        addressEditImage = view.findViewById(R.id.addressEdit);
+        nameEditImage = view.findViewById(R.id.nameEdit);
+        surnameEditImage = view.findViewById(R.id.surnameEdit);
+        phoneEditImage = view.findViewById(R.id.phoneEdit);
+        emailEditImage = view.findViewById(R.id.emailEdit);
+        btnSavePassenger = view.findViewById(R.id.btnSavePassenger);
+    }
+
+    class EditImageListener implements View.OnClickListener {
 
         private final int etId;
         private final View v;
 
-        public EditListener(View v, int etId) {
+        public EditImageListener(View v, int etId) {
             this.etId = etId;
             this.v = v;
         }
@@ -80,6 +128,7 @@ public class PassengerAccountFragment extends Fragment {
         public void onClick(View view) {
             EditText etAddress =  v.findViewById(etId);
             etAddress.setEnabled(true);
+            btnSavePassenger.setVisibility(View.VISIBLE);
         }
     }
 }
