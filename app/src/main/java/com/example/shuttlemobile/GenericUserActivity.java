1935f03ac7ce79ee31623af0a977ff05b092717b
magley <magazinlazar2@gmail.com>
2022-11-22 10:38:55 +0100

    feat: Add user activity class

---
+++ app/src/main/java/com/example/shuttlemobile/GenericUserActivity.java
@@ -0,0 +1,51 @@
+package com.example.shuttlemobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+/**
+ * <p>
+ *     GenericUserActivity is an empty abstract activity with an empty toolbar.<br/>
+ *     The toolbar has a back button.
+ *     The toolbar is initialized in <b>onAttachedToWindow()</b> instead of onCreate().
+ * </p>
+ * <p>
+ *     Override <b>toolbarOnItemClick()</b> for custom toolbar events.
+ * </p>
+ */
+public class GenericUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generic_user);
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setOnMenuItemClickListener(item -> toolbarOnItemClick(item));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected boolean toolbarOnItemClick(MenuItem item) {
+        final int itemId = item.getItemId();
+        return false;
+    }
+}
--- app/src/main/java/com/example/shuttlemobile/passenger/PassengerActivity.java
+++ app/src/main/java/com/example/shuttlemobile/passenger/PassengerActivity.java
@@ -6,35 +6,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.shuttlemobile.GenericUserActivity;
 import com.example.shuttlemobile.R;
 
-public class PassengerActivity extends AppCompatActivity {
+public class PassengerActivity extends GenericUserActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        toolbar.setOnMenuItemClickListener(item -> toolbarOnItemClick(item));
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private boolean toolbarOnItemClick(MenuItem item) {
-        final int itemId = item.getItemId();
-        return false;
     }
 }
