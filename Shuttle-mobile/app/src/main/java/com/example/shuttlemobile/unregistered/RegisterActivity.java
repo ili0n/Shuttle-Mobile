dbd7bb936de9707b0cbea9ebfe8009aaba8cabf0
magley <magazinlazar2@gmail.com>
2022-12-05 20:01:48 +0100

    feat: Add register layout

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/unregistered/RegisterActivity.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/unregistered/RegisterActivity.java
@@ -2,10 +2,13 @@ package com.example.shuttlemobile.unregistered;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.SimpleToolbarActivity;
+import com.google.android.material.appbar.AppBarLayout;
 
 public class RegisterActivity extends SimpleToolbarActivity {
 
