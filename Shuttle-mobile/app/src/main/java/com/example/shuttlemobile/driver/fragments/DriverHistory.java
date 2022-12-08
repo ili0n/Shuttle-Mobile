7f613e5328dab9ce1dfdd6a6a219c9d35088820a
magley <magazinlazar2@gmail.com>
2022-12-08 16:59:24 +0100

    feat: Create DriverHistory layout

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/fragments/DriverHistory.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/fragments/DriverHistory.java
@@ -1,14 +1,26 @@
 package com.example.shuttlemobile.driver.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
 import com.example.shuttlemobile.common.SessionContext;
+import com.example.shuttlemobile.common.adapter.EasyListAdapter;
+import com.example.shuttlemobile.passenger.fragments.PassengerHistory;
+import com.example.shuttlemobile.passenger.subactivities.PassengerHistoryDetailsActivity;
+import com.example.shuttlemobile.ride.Ride;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DriverHistory extends GenericUserFragment {
     public static DriverHistory newInstance(SessionContext session) {
@@ -22,6 +34,59 @@ public class DriverHistory extends GenericUserFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_driver_history, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        initializeList();
+    }
+
+    private void initializeList() {
+        ListView listView = getActivity().findViewById(R.id.list_d_history);
+
+        List<Ride> rides = new ArrayList<>();
+        rides.add(new Ride());
+        rides.add(new Ride());
+        rides.add(new Ride());
+        rides.add(new Ride());
+        rides.add(new Ride());
+        rides.add(new Ride());
+        rides.add(new Ride());
+
+        listView.setAdapter(new EasyListAdapter<Ride>() {
+            @Override
+            public List<Ride> getList() { return rides; }
+            @Override
+            public LayoutInflater getLayoutInflater() { return DriverHistory.this.getLayoutInflater(); }
+            @Override
+            public int getListItemLayoutId() { return R.layout.list_d_history; }
+            @Override
+            public void applyToView(View view, Ride obj) {
+                TextView routeA = view.findViewById(R.id.list_d_history_route_A);
+                TextView routeB = view.findViewById(R.id.list_d_history_route_B);
+                TextView date = view.findViewById(R.id.list_d_history_date);
+                TextView time = view.findViewById(R.id.list_d_history_time);
+                TextView cost = view.findViewById(R.id.list_d_history_cost);
+                TextView driverFullName = view.findViewById(R.id.list_d_history_pname);
+                ImageView driverPfp = view.findViewById(R.id.list_d_history_ppfp);
+
+                //passengerName.setText(obj.getPassenger().getName());
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Ride obj = (Ride)listView.getItemAtPosition(i);
+                openRideDetailsActivity(obj);
+            }
+        });
+    }
+
+    private void openRideDetailsActivity(Ride ride) {
+        Intent intent = new Intent(getActivity(), PassengerHistoryDetailsActivity.class);
+        intent.putExtra(PassengerHistoryDetailsActivity.PARAM_SESSION, session);
+        intent.putExtra(PassengerHistoryDetailsActivity.PARAM_RIDE, ride);
+        startActivity(intent);
     }
 
 }
