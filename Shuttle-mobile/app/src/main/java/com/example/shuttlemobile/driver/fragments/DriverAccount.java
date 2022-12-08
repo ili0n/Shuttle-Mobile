781362f9d7860dae6815da78dc37584b63c46e5e
magley <magazinlazar2@gmail.com>
2022-12-08 16:42:02 +0100

    feat: Create GenericUserTabbedFragment
    All tabbed fragments extend this class, making the code cleaner.

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/fragments/DriverAccount.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/fragments/DriverAccount.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
+import com.example.shuttlemobile.common.GenericUserTabbedFragment;
 import com.example.shuttlemobile.common.SessionContext;
 import com.example.shuttlemobile.passenger.fragments.PassengerAccountFavorites;
 import com.example.shuttlemobile.passenger.fragments.PassengerAccountInfo;
@@ -21,11 +22,7 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DriverAccount extends GenericUserFragment {
-    TabLayout tabLayout;
-    FragmentContainerView fragContainer;
-    List<Fragment> fragments = new ArrayList<>();
-
+public class DriverAccount extends GenericUserTabbedFragment {
     public static DriverAccount newInstance(SessionContext session) {
         DriverAccount fragment = new DriverAccount();
         Bundle bundle = new Bundle();
@@ -35,51 +32,24 @@ public class DriverAccount extends GenericUserFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_driver_account, container, false);
+    protected int getLayoutID() {
+        return R.layout.fragment_driver_account;
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        initMap();
-        buildTabLayout();
-        tabLayout.getTabAt(0).select();
-        setVisibleFragment(fragments.get(0));
+    protected int getTabLayoutID() {
+        return R.id.tab_d_acc;
     }
 
-    private void initMap() {
+    @Override
+    protected int getFragmentContainerID() {
+        return R.id.fragview_d_acc_tab;
+    }
+
+    @Override
+    protected void initFragmentList() {
         fragments.add(DriverAccountInfo.newInstance(session));
         fragments.add(DriverAccountStats.newInstance(session));
         fragments.add(DriverAccountReports.newInstance(session));
-    }
-
-    private void buildTabLayout() {
-        tabLayout = getActivity().findViewById(R.id.tab_d_acc);
-        fragContainer = getActivity().findViewById(R.id.fragview_d_acc_tab);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                final int index = tab.getPosition();
-                setVisibleFragment(fragments.get(index));
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
-        });
-    }
-
-    protected final void setVisibleFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager()
-                .beginTransaction()
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .setReorderingAllowed(true)
-                .replace(fragContainer.getId(), fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
     }
 }
--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerAccount.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerAccount.java
@@ -15,6 +15,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
+import com.example.shuttlemobile.common.GenericUserTabbedFragment;
 import com.example.shuttlemobile.common.SessionContext;
 import com.google.android.material.tabs.TabLayout;
 
@@ -23,22 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * PassengerAccount fragment contains a <code>TabLayout</code> with 3 tabs:
- * <br/>
- * <ol>
- *     <li>Account Info</li>
- *     <li>Favorite Routes</li>
- *     <li>Statistics</li>
- * </ol>
- * Each of those is a separate fragment shown in <code>fragContainer</code>
- * and stored in the <code>fragments</code> list.
- */
-public class PassengerAccount extends GenericUserFragment {
-    TabLayout tabLayout;
-    FragmentContainerView fragContainer;
-    List<Fragment> fragments = new ArrayList<>();
-
+public class PassengerAccount extends GenericUserTabbedFragment {
     public static PassengerAccount newInstance(SessionContext session) {
         PassengerAccount fragment = new PassengerAccount();
         Bundle bundle = new Bundle();
@@ -48,55 +34,24 @@ public class PassengerAccount extends GenericUserFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_passenger_account, container, false);
+    protected int getLayoutID() {
+        return R.layout.fragment_passenger_account;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        initMap();
-        buildTabLayout();
-        tabLayout.getTabAt(0).select();
-        setVisibleFragment(fragments.get(0));
+    protected int getTabLayoutID() {
+        return R.id.tab_p_acc;
     }
 
-    private void initMap() {
-        // We can't use TabItem id's here since they technically don't have IDs.
-        // getId() would always return -1. Therefore we use getPosition() and lists.
-        // Be careful about the element order.
+    @Override
+    protected int getFragmentContainerID() {
+        return R.id.fragview_p_acc_tab;
+    }
+
+    @Override
+    protected void initFragmentList() {
         fragments.add(PassengerAccountInfo.newInstance(session));
         fragments.add(PassengerAccountFavorites.newInstance(session));
         fragments.add(PassengerAccountStats.newInstance(session));
-    }
-
-    private void buildTabLayout() {
-        tabLayout = getActivity().findViewById(R.id.tab_p_acc);
-        fragContainer = getActivity().findViewById(R.id.fragview_p_acc_tab);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                final int index = tab.getPosition();
-                setVisibleFragment(fragments.get(index));
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
-        });
-    }
-
-
-    protected final void setVisibleFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager()
-                .beginTransaction()
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .setReorderingAllowed(true)
-                .replace(fragContainer.getId(), fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
     }
 }
