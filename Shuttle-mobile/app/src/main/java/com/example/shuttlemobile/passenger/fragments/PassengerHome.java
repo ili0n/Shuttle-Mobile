e339e8456d6437b7049e2ac0b11fd524ce9ff9fc
magley <magazinlazar2@gmail.com>
2022-12-17 16:56:48 +0100

    feat: Implement map annotations
    It seems that there isn't an easy way to color vector drawables.

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerHome.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerHome.java
@@ -1,5 +1,11 @@
 package com.example.shuttlemobile.passenger.fragments;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
@@ -8,12 +14,25 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
 import com.example.shuttlemobile.common.SessionContext;
+import com.mapbox.geojson.Point;
 import com.mapbox.maps.MapView;
 import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.annotation.AnnotationConfig;
+import com.mapbox.maps.plugin.annotation.AnnotationPlugin;
+import com.mapbox.maps.plugin.annotation.AnnotationPluginImplKt;
+import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationManager;
+import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationManagerKt;
+import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationOptions;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManagerKt;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions;
+import com.mapbox.maps.viewannotation.ViewAnnotationManager;
 
 public class PassengerHome extends GenericUserFragment {
     private MapView mapView;
@@ -35,7 +54,64 @@ public class PassengerHome extends GenericUserFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mapView = getActivity().findViewById(R.id.mapView);
-        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                addPinToMap();
+            }
+        });
+    }
+
+    private void addPinToMap() {
+        drawCircleToMap(Point.fromLngLat(0, 0), 8.0, "#ff0000");
+
+        drawCar(Point.fromLngLat(5, 0), true);
+        drawCar(Point.fromLngLat(4, 2), true);
+        drawCar(Point.fromLngLat(2, -3), false);
+        drawCar(Point.fromLngLat(1, 1), false);
+
+    }
+
+    private void drawCar(Point pos, boolean available) {
+        if (available) {
+            drawAnnotationToMap(pos, R.drawable.car_green);
+        } else {
+            drawAnnotationToMap(pos, R.drawable.car_red);
+        }
+    }
+
+    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    private void drawCircleToMap(Point point, Double radius, String hexColor) {
+        AnnotationPlugin annotationApi = AnnotationPluginImplKt.getAnnotations(mapView);
+        CircleAnnotationManager circleAnnotationManager = CircleAnnotationManagerKt.createCircleAnnotationManager(annotationApi, new AnnotationConfig());
+        CircleAnnotationOptions circleAnnotationOptions = new CircleAnnotationOptions()
+                .withPoint(point)
+                .withCircleRadius(radius)
+                .withCircleColor(hexColor)
+                .withCircleStrokeWidth(2.0)
+                .withCircleStrokeColor("#ffffff")
+        ;
+        circleAnnotationManager.create(circleAnnotationOptions);
+    }
+
+    private void drawAnnotationToMap(Point point, int drawable) {
+        AnnotationPlugin annotationApi = AnnotationPluginImplKt.getAnnotations(mapView);
+        PointAnnotationManager pointAnnotationManager = PointAnnotationManagerKt.createPointAnnotationManager(annotationApi, new AnnotationConfig());
+        PointAnnotationOptions pointAnnotationOptions = new PointAnnotationOptions()
+                .withPoint(point)
+                .withIconImage(getBitmapFromVectorDrawable(getActivity(), drawable))
+        ;
+        pointAnnotationManager.create(pointAnnotationOptions);
     }
 
     @Override
