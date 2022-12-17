f64dfa75736483cca87745b59ec59b9840262913
magley <magazinlazar2@gmail.com>
2022-12-17 12:27:29 +0100

    feat: Ask for location on startup

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/MainActivity.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.shuttlemobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.shuttlemobile.passenger.PassengerActivity;
 import com.example.shuttlemobile.unregistered.LoginActivity;
@@ -12,6 +25,11 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    private final int PERMISSION_LOCATION = 1;
+
+    private boolean hasPermission(String permission) {
+        return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +39,52 @@ public class MainActivity extends AppCompatActivity {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                finish();
+                checkForPermissions();
+                exitSplashScreen();
             }
         }, 3000);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                } else {
+                    // Permission Denied
+                    finishAndRemoveTask();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Check and ask for all permissions that are required ahead-of-time.
+     * If the user declines any permission, the app will close.
+     */
+    private void checkForPermissions() {
+        checkForLocationPermission();
+        // checkForInternetPermission(); TODO
+    }
+
+    private void checkForLocationPermission() {
+        if (!hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+        && !hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            String[] permissions = new String[] {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            };
+
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_LOCATION);
+        }
+    }
+
+    private void exitSplashScreen() {
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
     }
 }
