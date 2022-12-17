ed04c6a36438f686a9faed0610ca6191b33dbc4b
magley <magazinlazar2@gmail.com>
2022-12-17 15:10:17 +0100

    feat: Add mapbox mapview into PassengerHome

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerHome.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerHome.java
@@ -6,11 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
 import com.example.shuttlemobile.common.SessionContext;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 public class PassengerHome extends GenericUserFragment {
+    private MapView mapView;
+
     public static PassengerHome newInstance(SessionContext session) {
         PassengerHome fragment = new PassengerHome();
         Bundle bundle = new Bundle();
@@ -22,5 +29,36 @@ public class PassengerHome extends GenericUserFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_passenger_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = getActivity().findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
     }
 }
