113f4ec634818f8935b1eb305ad516b93fbc5d5e
uros <urosstanic25@gmail.com>
2023-01-12 18:54:03 +0100

    filled order ride spinners

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/orderride/fragments/RidePropertiesFragment.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/orderride/fragments/RidePropertiesFragment.java
@@ -7,10 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
 import com.example.shuttlemobile.common.SessionContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,6 +36,7 @@ public class RidePropertiesFragment extends Fragment {
     public RidePropertiesFragment() {
         // Required empty public constructor
     }
+
     // TODO: Rename and change types and number of parameters
     public static RidePropertiesFragment newInstance(SessionContext session) {
         RidePropertiesFragment fragment = new RidePropertiesFragment();
@@ -53,6 +59,25 @@ public class RidePropertiesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ride_properties, container, false);
+        View view = inflater.inflate(R.layout.fragment_ride_properties, container, false);
+        setSpinnerItems(view);
+
+        return view;
+    }
+
+    private void setSpinnerItems(View view) {
+        Spinner spinner = (Spinner) view.findViewById(R.id.vehicle_type_spinner);
+        List<String> types = getVehicleTypes();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, types);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    List<String> getVehicleTypes() {
+        List<String> list = new ArrayList<>();
+        list.add("STANDARD");
+        list.add("VAN");
+        list.add("LUXURY");
+        return list;
     }
 }
--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/orderride/fragments/ScheduleRide.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/orderride/fragments/ScheduleRide.java
@@ -7,10 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
 import com.example.shuttlemobile.common.SessionContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +24,8 @@ import com.example.shuttlemobile.common.SessionContext;
  * create an instance of this fragment.
  */
 public class ScheduleRide extends Fragment {
-
+    private static final int MAX_HOURS = 5;
+    private static final int MAX_MINUTES = 60;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -54,6 +61,42 @@ public class ScheduleRide extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule_ride, container, false);
+        View view = inflater.inflate(R.layout.fragment_schedule_ride, container, false);
+        setHourSpinnerItems(view);
+        setMinuteSpinnerItems(view);
+        return view;
+    }
+
+
+    private void setMinuteSpinnerItems(View view) {
+        Spinner spinner = (Spinner) view.findViewById(R.id.minute_spinner);
+        List<Integer> hours = getMinuteValues();
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getContext(), android.R.layout.simple_spinner_item, hours);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    List<Integer> getMinuteValues() {
+        List<Integer> minutes = new ArrayList<>();
+        for (int i = 0; i < MAX_MINUTES; i += 5) {
+            minutes.add(i);
+        }
+        return minutes;
+    }
+
+    private void setHourSpinnerItems(View view) {
+        Spinner spinner = (Spinner) view.findViewById(R.id.hour_spinner);
+        List<Integer> hours = getHourValues();
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getContext(), android.R.layout.simple_spinner_item, hours);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    List<Integer> getHourValues() {
+        List<Integer> hours = new ArrayList<>();
+        for (int i = 0; i < MAX_HOURS; i++) {
+            hours.add(i);
+        }
+        return hours;
     }
 }
