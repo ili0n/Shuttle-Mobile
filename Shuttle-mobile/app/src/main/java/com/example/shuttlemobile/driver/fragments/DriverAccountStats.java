9064858a6a4659b254489ebf96392d590d76d62e
uros <urosstanic25@gmail.com>
2023-01-15 22:25:25 +0100

    added driver stats fragment gui

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/fragments/DriverAccountStats.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/driver/fragments/DriverAccountStats.java
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.shuttlemobile.R;
 import com.example.shuttlemobile.common.GenericUserFragment;
 import com.example.shuttlemobile.common.SessionContext;
+import com.example.shuttlemobile.driver.DriverStatsDTO;
 import com.example.shuttlemobile.passenger.dto.GraphEntryDTO;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
@@ -35,9 +38,32 @@ public class DriverAccountStats extends GenericUserFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_driver_account_stats, container, false);
+        EditText rejections = view.findViewById(R.id.d_stat_rejection);
+        EditText rides = view.findViewById(R.id.d_stat_rides);
+        EditText hours = view.findViewById(R.id.d_stat_hours);
+        EditText money = view.findViewById(R.id.d_stat_money);
+
+        Button submit = view.findViewById(R.id.d_stat_submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO request
+                DriverStatsDTO dto = new DriverStatsDTO(10, 10, 100, 2500);
+
+                setData(rejections, rides, hours, money, dto);
+            }
+        });
+
         return view;
     }
 
+    private void setData(EditText rejections, EditText rides, EditText hours, EditText money, DriverStatsDTO statsDTO) {
+        rejections.setText(statsDTO.getRejections() + "");
+        rides.setText(statsDTO.getRides() + "");
+        hours.setText(statsDTO.getHours() + "");
+        money.setText(statsDTO.getMoney() + "");
+    }
 
 
 }
