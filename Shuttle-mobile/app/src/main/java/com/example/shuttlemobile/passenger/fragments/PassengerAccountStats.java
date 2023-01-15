91ba8553fa0b29ed5c995af77eac7aab8a0c6f2e
SV 10/2020 <nemanja.majstorovic3214@gmail.com>
2023-01-15 14:01:18 +0100

    Added line plotting for all sets

--- Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerAccountStats.java
+++ Shuttle-mobile/app/src/main/java/com/example/shuttlemobile/passenger/fragments/PassengerAccountStats.java
@@ -12,16 +12,22 @@ import com.example.shuttlemobile.common.GenericUserFragment;
 import com.example.shuttlemobile.common.SessionContext;
 import com.example.shuttlemobile.passenger.dto.GraphEntryDTO;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 
 public class PassengerAccountStats extends GenericUserFragment {
-    private BarChart chart;
+    private LineChart chart;
 
     public static PassengerAccountStats newInstance(SessionContext session) {
         PassengerAccountStats fragment = new PassengerAccountStats();
@@ -45,48 +51,65 @@ public class PassengerAccountStats extends GenericUserFragment {
     }
 
     public void createGraph(View v){
-        chart = v.findViewById(R.id.chart1);
-
-        chart.setDrawBarShadow(false);
-        chart.setDrawValueAboveBar(true);
+        chart = v.findViewById(R.id.p_chart);
         chart.getDescription().setEnabled(false);
         chart.setDrawGridBackground(false);
-
-//        Legend l = chart.getLegend();
-//        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-//        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
-//        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-//        l.setDrawInside(false);
-//        l.setForm(Legend.LegendForm.SQUARE);
-//        l.setFormSize(9f);
-//        l.setTextSize(11f);
-//        l.setXEntrySpace(4f);
     }
     private void setData() {
         ArrayList<GraphEntryDTO> entries = mockEntries();
-        ArrayList<BarEntry> values = new ArrayList<>();
+        ArrayList<Entry> costSum = new ArrayList<>();
+        ArrayList<Entry> totalLength = new ArrayList<>();
+        ArrayList<Entry> numOfRides = new ArrayList<>();
         for (int i = (int) 0; i < entries.size(); i++) {
-            values.add(new BarEntry((float) i, entries.get(i).getCostSum().floatValue()));
+            costSum.add(new Entry((float) i, entries.get(i).getCostSum().floatValue()));
+            totalLength.add(new Entry((float) i, entries.get(i).getLength().floatValue()));
+            numOfRides.add(new Entry((float) i, entries.get(i).getNumberOfRides().floatValue()));
         }
 
-        BarDataSet set1;
+        LineDataSet costSumSet;
+        LineDataSet totalLengthSet;
+        LineDataSet numOfRidesSet;
 //        change if data already exists
         if (chart.getData() != null &&
                 chart.getData().getDataSetCount() > 0) {
-            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
-            set1.setValues(values);
+
+            costSumSet = (LineDataSet) chart.getData().getDataSetByIndex(0);
+            costSumSet.setValues(costSum);
+
+            totalLengthSet = (LineDataSet) chart.getData().getDataSetByIndex(1);
+            totalLengthSet.setValues(totalLength);
+
+            numOfRidesSet = (LineDataSet) chart.getData().getDataSetByIndex(2);
+            numOfRidesSet.setValues(numOfRides);
+
             chart.getData().notifyDataChanged();
             chart.notifyDataSetChanged();
 //        initial setting of data
         } else {
             setAxis(entries);
-            set1 = new BarDataSet(values, "Passenger stats");
-            set1.setDrawIcons(false);
-            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-            dataSets.add(set1);
-            BarData data = new BarData(dataSets);
+            costSumSet = new LineDataSet(costSum, "Cost sum");
+            costSumSet.setColors(new int[] { R.color.purple_200 }, requireContext());
+            costSumSet.setDrawIcons(false);
+
+            totalLengthSet = new LineDataSet(totalLength, "Total length");
+            totalLengthSet.setColors(new int[] { R.color.red }, requireContext());
+            setAxis(entries);
+            totalLengthSet.setDrawIcons(false);
+
+            numOfRidesSet = new LineDataSet(numOfRides, "Number of rides");
+            numOfRidesSet.setColors(new int[] {R.color.green }, requireContext());
+            setAxis(entries);
+            numOfRidesSet.setDrawIcons(false);
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(costSumSet);
+            dataSets.add(totalLengthSet);
+            dataSets.add(numOfRidesSet);
+
+
+            LineData data = new LineData(dataSets);
             data.setValueTextSize(10f);
-            data.setBarWidth(0.9f);
+//            data.setBarWidth(0.9f);
             chart.setData(data);
         }
     }
@@ -94,9 +117,9 @@ public class PassengerAccountStats extends GenericUserFragment {
     private ArrayList<GraphEntryDTO> mockEntries() {
         ArrayList<GraphEntryDTO> entries = new ArrayList<>();
         GraphEntryDTO g1 = new GraphEntryDTO("asd", 1L, (double) 2.0, 3.0);
-        GraphEntryDTO g2 = new GraphEntryDTO("efw", 1L, (double) 7.0, 3.0);
-        GraphEntryDTO g3 = new GraphEntryDTO("xcv", 1L, (double) 3.0, 3.0);
-        GraphEntryDTO g4 = new GraphEntryDTO("qwr", 1L, (double) 1.0, 3.0);
+        GraphEntryDTO g2 = new GraphEntryDTO("efw", 3L, (double) 7.0, 9.0);
+        GraphEntryDTO g3 = new GraphEntryDTO("xcv", 4L, (double) 3.0, 10.0);
+        GraphEntryDTO g4 = new GraphEntryDTO("qwr", 5L, (double) 1.0, 2.0);
         entries.add(g1);
         entries.add(g2);
         entries.add(g3);
